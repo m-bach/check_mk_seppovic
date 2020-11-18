2b64f7086d9cbfa61a8beac433653295d26d51da
Robert Sander <r.sander@heinlein-support.de>
2020-11-18 17:36:50 +0100

    renames metrics for more consistency
    uses slapd_ prefix or already existing metric names

---
+++ slapd/web/plugins/metrics/slapd.py
@@ -0,0 +1,89 @@
+metric_info["slapd_abandon"] = {
+    "title": _("Abandon Ops completed"),
+    "unit": "1/s",
+    "color": "12/a",
+}
+
+metric_info["slapd_add"] = {
+    "title": _("Add Ops completed"),
+    "unit": "1/s",
+    "color": "14/a",
+}
+
+metric_info["slapd_bind"] = {
+    "title": _("Bind Ops completed"),
+    "unit": "1/s",
+    "color": "16/a",
+}
+
+metric_info["slapd_compare"] = {
+    "title": _("Compare Ops completed"),
+    "unit": "1/s",
+    "color": "22/a",
+}
+
+metric_info["slapd_delete"] = {
+    "title": _("Delete Ops completed"),
+    "unit": "1/s",
+    "color": "24/a",
+}
+
+metric_info["slapd_extended"] = {
+    "title": _("Extended Ops completed"),
+    "unit": "1/s",
+    "color": "26/a",
+}
+
+metric_info["slapd_modify"] = {
+    "title": _("Modify Ops completed"),
+    "unit": "1/s",
+    "color": "32/a",
+}
+
+metric_info["slapd_modrdn"] = {
+    "title": _("ModRDN Ops completed"),
+    "unit": "1/s",
+    "color": "34/a",
+}
+
+metric_info["slapd_search"] = {
+    "title": _("Search Ops completed"),
+    "unit": "1/s",
+    "color": "36/a",
+}
+
+metric_info["slapd_unbind"] = {
+    "title": _("Unbind Ops completed"),
+    "unit": "1/s",
+    "color": "42/a",
+}
+
+metric_info["slapd_entries_sent"] = {
+    "title": _("Entries sent"),
+    "unit": "1/s",
+    "color": "23/a",
+}
+
+metric_info["slapd_pdu_sent"] = {
+    "title": _("PDUs sent"),
+    "unit": "1/s",
+    "color": "33/a",
+}
+
+metric_info["slapd_referrals_sent"] = {
+    "title": _("Referrals sent"),
+    "unit": "1/s",
+    "color": "43/a",
+}
+
+metric_info["slapd_waiters_read"] = {
+    "title": _("Read Waiters"),
+    "unit": "count",
+    "color": "25/a",
+}
+
+metric_info["slapd_waiters_write"] = {
+    "title": _("Write Waiters"),
+    "unit": "count",
+    "color": "35/a",
+}

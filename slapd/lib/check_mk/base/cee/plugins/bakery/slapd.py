ef8e68a0f05c5fbbb327d26eb103832054cccf61
Markus Bach <m.bach@pegasusgmbh.de>
2021-08-12 10:44:50 +0200

    slapd: add missing , to syncrepl config in bakery

--- slapd/lib/check_mk/base/cee/plugins/bakery/slapd.py
+++ slapd/lib/check_mk/base/cee/plugins/bakery/slapd.py
@@ -34,7 +34,7 @@ def get_slapd_files(conf: Dict[str, Any]) -> FileGenerator:
                 for syncreplconf in value:
                     content += ["      {"]
                     for key, value in syncreplconf.items():
-                        content += ["      '%s' => '%s'" % (key, value)]
+                        content += ["      '%s' => '%s'," % (key, value)]
                     content += ["      },"]
                 content += ["    ],"]
             else:
